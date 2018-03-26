d5cdfa71bc0cd4fafe698ee37cb007b4881860cb
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-03-25 22:18:30 -0400

    refactored to use supervisord images as base

--- build/usr/local/crf/startup/phpadmin.sh
+++ build/usr/local/crf/startup/phpadmin.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 if [ ! -f /etc/phpmyadmin/config.secret.inc.php ] ; then
     cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
 <?php
@@ -12,11 +13,8 @@ fi
 
 mkdir -p /var/nginx/client_body_temp
 chown nobody:nobody /sessions /var/nginx/client_body_temp
-mkdir -p /var/run/php/
-chown nobody:nobody /var/run/php/
-touch /var/log/php-fpm.log
-chown nobody:nobody /var/log/php-fpm.log
 
-if [ "$1" = 'phpmyadmin' ]; then
-    exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
-fi
+mkdir -p /var/run/php/
+touch /var/log/php-fpm.log
+
+chown nobody:nobody -R /var/run/php
