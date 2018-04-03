e3f7b6864b3b3099d73bc535210da60497d784e7
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-04-02 21:45:36 -0400

    refactored to include php5, supervisord and latest CBF

--- build/usr/local/crf/startup/logs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find /var/log -type d -exec chmod 777 '{}' \;
-find /var/log -type f ! -name '.*' -exec chmod 666 '{}' \; 
--- build/usr/local/crf/startup/php.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#touch "${RUN_DIR}/php5-fpm.sock"
-
-find "$SESSIONS_DIR" -type d -exec chmod 777 '{}' \;
-find "$SESSIONS_DIR" -type f -exec chmod 666 '{}' \;
-
-find "$RUN_DIR" -type d -exec chmod 777 '{}' \;
-find "$RUN_DIR" -type f -exec chmod 666 '{}' \;
